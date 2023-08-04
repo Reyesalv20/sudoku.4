dd41f43fe47ee373f9d21c9af1edc1b4767e3a81
Reyesalv20 <124184473+Reyesalv20@users.noreply.github.com>
2023-08-04 15:52:31 -0600

    inicio

---
+++ src/gamesuduku/GameSuduku.java
@@ -0,0 +1,18 @@
+
+package gamesuduku;
+
+/**
+ *
+ * @author ruthreyes
+ */
+public class GameSuduku {
+
+   
+    public static void main(String[] args) {
+          
+       soduku obj=new soduku();
+       obj.CreateBoard();
+        
+    }
+    
+}
